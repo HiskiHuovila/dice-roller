cc99726a914af92713e2ad3e410a95d56f0b2567
Hiski Huovila <hiski.huovila@gmail.com>
2024-11-17 18:45:48 +0200

    removed redundant script

--- src/project.ts
+++ src/project.ts
@@ -1,8 +1,7 @@
 import {makeProject} from '@motion-canvas/core';
 
-import example from './scenes/example?scene';
 import dice from './scenes/dice?scene';
 
 export default makeProject({
-  scenes: [dice,example],
+  scenes: [dice],
 });
